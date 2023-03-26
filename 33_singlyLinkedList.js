7e4aeb2f3fd26d6907fdf4a11146449ec8aae993
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-03-26 16:03:35 -0400

    set up unshift

--- 33_singlyLinkedList.js
+++ 33_singlyLinkedList.js
@@ -63,6 +63,21 @@ class SinglyLinkedList {
 
 		return currentHead;
 	}
+
+	unshift(val) {
+		const newNode = new Node(val);
+
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+
+		this.length++;
+		return this;
+	}
 }
 
 let list = new SinglyLinkedList();
