ba03192a20c788dff06cd7dcee74f762ac163f9d
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-04-05 01:04:53 -0400

    added unshift method to the doublyLinkedList

--- 34_doublyLinkedList.js
+++ 34_doublyLinkedList.js
@@ -72,4 +72,21 @@ class DoublyLinkedList {
 
 		return oldHead;
 	}
+
+	unshift(val) {
+		const newNode = new Node(val);
+
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+
+		this.length++;
+
+		return this;
+	}
 }
