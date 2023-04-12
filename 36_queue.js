29c883c9eaa0f8ce0fb030e3c1d804ae435afe0b
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-04-12 02:01:26 -0400

    added queue data structure

---
+++ 36_queue.js
@@ -0,0 +1,54 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+
+	enqueque(val) {
+		const newNode = new Node(val);
+
+		if (this.size === 0) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+
+		this.size++;
+
+		return this;
+	}
+
+	dequeue() {
+		if (this.size === 0) return null;
+
+		const removedNode = this.first;
+
+		if (this.size === 1) {
+			this.first = null;
+			this.last = null;
+		} else {
+			this.first = removedNode.next;
+		}
+
+		this.size--;
+
+		return removedNode;
+	}
+}
+
+const queue = new Queue();
+
+queue.enqueque(1);
+queue.enqueque(2);
+queue.enqueque(3);
+queue.enqueque(4);
