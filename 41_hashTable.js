fb37255e947f09424e19a72506b642b8b3ed41e1
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-05-02 00:41:13 -0400

    init hash table class

--- 41_hashTable.js
+++ 41_hashTable.js
@@ -1,12 +1,18 @@
-function hash(key, arrayLen) {
-	let total = 0;
-	let WEIRD_PRIME = 31;
-
-	for (let i = 0; i < Math.min(key.length, 100); i++) {
-		const char = key[i];
-		const value = char.charCodeAt(0) - 96;
-		total = (total * WEIRD_PRIME + value) % arrayLen;
+class HashTable {
+	constructor(size = 53) {
+		this.keyMap = new Array(size);
 	}
 
-	return total;
+	_hash(key) {
+		let total = 0;
+		let WEIRD_PRIME = 31;
+
+		for (let i = 0; i < Math.min(key.length, 100); i++) {
+			const char = key[i];
+			const value = char.charCodeAt(0) - 96;
+			total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+		}
+
+		return total;
+	}
 }
