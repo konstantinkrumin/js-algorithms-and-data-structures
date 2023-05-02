e97e01f900092c48c675002ba04a461e1dfa640d
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-05-02 01:06:59 -0400

    set up set method on hashtable

--- 41_hashTable.js
+++ 41_hashTable.js
@@ -15,4 +15,36 @@ class HashTable {
 
 		return total;
 	}
+
+	set(key, value) {
+		const index = this._hash(key);
+
+		if (!this.keyMap[index]) {
+			this.keyMap[index] = [];
+		} else {
+			this.keyMap[index].push([key, value]);
+		}
+
+		return index;
+	}
+
+	get(key) {
+		const hashedKey = this._hash(key);
+
+		return this.keyMap[hashedKey].filter(mapKey => mapKey === key);
+	}
 }
+
+const hashTable = new HashTable();
+
+// hashTable.set('pink', '#FF80ED');
+// hashTable.set('yellow', '#FFD700');
+// hashTable.set('turquoise', '#00FFFF');
+// hashTable.set('blue', '#0000FF');
+// hashTable.set('gray', '#696969');
+
+// hashTable.get('blue');
+// hashTable.set('turquoise');
+// hashTable.get('yellow');
+// hashTable.get('red');
+// hashTable.get('orange');
