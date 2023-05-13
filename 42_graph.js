68502adf9f180a855c16d984673218a412bd0065
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-05-13 18:28:57 -0400

    added methods to handle manipulations with nodes and edges

--- 42_graph.js
+++ 42_graph.js
@@ -8,4 +8,47 @@ class Graph {
 			this.adjacencyList[vertex] = [];
 		}
 	}
+
+	addEdge(vertex1, vertex2) {
+		this.adjacencyList[vertex1].push(vertex2);
+		this.adjacencyList[vertex2].push(vertex1);
+	}
+
+	removeEdge(vertex1, vertex2) {
+		this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+			currVal => currVal !== vertex2
+		);
+		this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+			currVal => currVal !== vertex1
+		);
+	}
+
+	removeVertex(vertex) {
+		const edges = this.adjacencyList[vertex];
+
+		for (let i = 0; i < edges.length; i++) {
+			const currEdge = edges[i];
+			this.removeEdge(vertex, currEdge);
+		}
+
+		delete this.adjacencyList[vertex];
+	}
 }
+
+const graph = new Graph();
+
+// graph.addVertex('Madrid');
+// graph.addVertex('Paris');
+// graph.addVertex('Moscow');
+// graph.addVertex('London');
+// graph.addVertex('Berlin');
+
+// graph.addEdge('Madrid', 'Paris');
+// graph.addEdge('Paris', 'London');
+// graph.addEdge('Berlin', 'London');
+// graph.addEdge('Paris', 'Berlin');
+// graph.addEdge('Madrid', 'London');
+
+// graph.removeEdge('Paris', 'London')
+
+// graph.removeVertex("London")
