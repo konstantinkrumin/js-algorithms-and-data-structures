90c8f5f258e88ef36048c4d00f5e00f342ccfc67
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-05-22 18:54:18 -0400

    added breadthFirstTraversal fn

--- 43_graph-traversal.js
+++ 43_graph-traversal.js
@@ -83,6 +83,29 @@ class Graph {
 
 		return result;
 	}
+
+	breadthFirstTraversal(start) {
+		let queue = [start];
+		let result = [];
+		let visited = {};
+
+		let currentVertex;
+		visited[start] = true;
+
+		while (queue.length) {
+			currentVertex = queue.shift();
+			result.push(currentVertex);
+
+			this.adjacencyList[currentVertex].forEach(neighbor => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					queue.push(neighbor);
+				}
+			});
+		}
+
+		return result;
+	}
 }
 
 const graph = new Graph();
