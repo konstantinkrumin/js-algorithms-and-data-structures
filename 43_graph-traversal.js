b40f742d2cc801fa19972961c10e9325164e20bf
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-05-22 17:37:08 -0400

    set up depthFirstTraversal function

---
+++ 43_graph-traversal.js
@@ -0,0 +1,80 @@
+class Graph {
+	constructor() {
+		this.adjacencyList = {};
+	}
+
+	addVertex(vertex) {
+		if (!this.adjacencyList[vertex]) {
+			this.adjacencyList[vertex] = [];
+		}
+	}
+
+	addEdge(vertex1, vertex2) {
+		this.adjacencyList[vertex1].push(vertex2);
+		this.adjacencyList[vertex2].push(vertex1);
+	}
+
+	removeEdge(vertex1, vertex2) {
+		this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+			currVal => currVal !== vertex2
+		);
+		this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+			currVal => currVal !== vertex1
+		);
+	}
+
+	removeVertex(vertex) {
+		const adjacentVertices = this.adjacencyList[vertex];
+
+		for (let i = 0; i < adjacentVertices.length; i++) {
+			const adjacentVertex = edges[i];
+			this.removeEdge(vertex, adjacentVertex);
+		}
+
+		delete this.adjacencyList[vertex];
+	}
+
+	depthFirstTraversal(start) {
+		let result = [];
+
+		let visited = {};
+
+		const helper = vertex => {
+			if (!vertex) return;
+
+			visited[vertex] = true;
+			result.push(vertex);
+
+			const adjacentVertices = this.adjacencyList[vertex];
+
+			for (let i = 0; i < adjacentVertices.length; i++) {
+				const currVal = adjacentVertices[i];
+
+				if (!visited[currVal]) {
+					helper(currVal);
+				}
+			}
+		};
+
+		helper(start);
+
+		return result;
+	}
+}
+
+const graph = new Graph();
+
+// graph.addVertex('A');
+// graph.addVertex('B');
+// graph.addVertex('C');
+// graph.addVertex('D');
+// graph.addVertex('E');
+// graph.addVertex('F');
+
+// graph.addEdge('A', 'B');
+// graph.addEdge('A', 'C');
+// graph.addEdge('B', 'D');
+// graph.addEdge('C', 'E');
+// graph.addEdge('D', 'E');
+// graph.addEdge('D', 'F');
+// graph.addEdge('E', 'F');
