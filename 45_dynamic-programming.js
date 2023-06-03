c6db1e476bf5103f137d1121abb2ee6dfc7777ab
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-06-03 00:52:56 -0400

    added Tabulated Fibonacci

--- 45_dynamic-programming.js
+++ 45_dynamic-programming.js
@@ -23,3 +23,15 @@ function fib(n, memo = [undefined, 1, 1]) {
 	memo[n] = res;
 	return res;
 }
+
+// Tabulated Fibonacci
+function fib(n) {
+	if (n <= 2) return 1;
+
+	let fibNums = [0, 1, 1];
+	for (let i = 3; i <= n; i++) {
+		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+	}
+
+	return fibNums[n];
+}
